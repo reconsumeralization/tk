4eca584a6760eab1ad8c555d081a3191355d2605
David Weatherspoon <reconsumeralization@gmail.com>
2023-12-06 18:00:12 -0500

    Please enter the commit message for your changes. Lines starting

--- components/StudentDashboard.tsx
+++ components/StudentDashboard.tsx
@@ -8,67 +8,20 @@ const useFetch = (url, data) => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const res = await axios.post(url, data);
-        setResponse(res.data);
-      } catch (error) {
-        setError(error);
-      }
-      setIsLoading(false);
-    };
-
-    fetchData();
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.post(url, data);
+      setResponse(res.data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
   }, [url, data]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return { response, error, isLoading };
 };
-
-// Component for displaying grades as a bar chart
-const GradesChart = ({ grades }) => {
-  const data = {
-    labels: Object.keys(grades),
-    datasets: [
-      {
-        label: 'Grades',
-        data: Object.values(grades),
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 1,
-      },
-    ],
-  };
-  return <Bar data={data} />;
-};
-
-const StudentDashboard: React.FC = () => {
-  // Replace this with actual data
-  const grades = {
-    Math: 85,
-    English: 90,
-    Science: 95,
-    History: 88,
-    Art: 92,
-  };
-
-  const { response: conversation, error, isLoading }: { response: any, error: any, isLoading: boolean } = useFetch('/conversation', { conversation: "Tell me about the test results." });
-  return (
-    <div>
-      <h1>Student Dashboard</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: {error.message}</p>
-      ) : (
-        <div>
-          <p>{conversation}</p>
-        </div>
-      )}
-      <GradesChart grades={grades} />
-    </div>
-  );
-};
-
-export default StudentDashboard;
